693173aa646fc591eb46907c919b1c7c2c6fa940
Jake Partusch <jakepartusch@gmail.com>
2019-01-01 22:10:21 -0600

    feat: enable offline, update icon

--- gatsby-config.js
+++ gatsby-config.js
@@ -18,17 +18,17 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `daily-mood-tracker`,
+        short_name: `Mood Tracker`,
         start_url: `/`,
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/smiley.svg`, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.app/offline
-    // 'gatsby-plugin-offline',
+    'gatsby-plugin-offline',
   ],
 }

5fbba31fde9ef6f7191a092f0d86fd24b34ac15e
Jake Partusch <jakepartusch@gmail.com>
2019-01-20 11:45:46 -0600

    fix: remove email reference

--- src/components/history/historyGrid.js
+++ src/components/history/historyGrid.js
@@ -12,8 +12,7 @@ class HistoryGrid extends Component {
     }
     
     async componentDidMount() {
-        const { email } = netlifyIdentity.currentUser();
-        const response = await this.getMoodsForUser(email);
+        const response = await this.getMoodsForUser();
         const moods = this.getMoodsMatchingMonth(response, moment().month());
         this.createSquaresForMonth(moods);
         this.setState({ moods });
